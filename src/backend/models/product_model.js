17927047b600961467276538dfd215fff8c255ef
Gandino1984 <andinogerman@gmail.com>
2024-11-11 00:15:04 +0100

    models done

---
+++ src/backend/models/product_model.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+
+import sequelize from "../config/sequelize.js";
+
+const product_model = sequelize.define("product", {
+    id_product: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name_product: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    price_product: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    discount_product: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false
+    },
+    season_product: {
+        type: DataTypes.STRING(45),
+        allowNull: false
+    }
+}, {
+    timestamps: false,
+    freezeTableName: true
+});
+
+export default product_model;
